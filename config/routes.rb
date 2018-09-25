15a0190a5e38f52fe2a52fb611b04e2f21bb43ee
Lucas Ramos <lucasrcdias@gmail.com>
2018-09-24 21:57:46 -0300

    [routes] Remove income and expenses route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: :create
       resources :categories, except: [:new, :edit, :show]
-      resources :entries, except: [:new, :edit, :show]
-
-      get '/incomes', to: 'entries#incomes', as: :incomes
-      get '/expenses', to: 'entries#expenses', as: :expenses
+      resources :entries, except: [:new, :edit, :show, :index]
 
       post '/sessions/new', to: 'sessions#new', as: :new_session
     end
